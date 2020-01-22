0e38186f6e51ebe78f9385de6c25fa9005e3791a
Joe <ckdfo1995@gmail.com>
2020-01-22 14:31:37 +0900

    add content-type

--- main.js
+++ main.js
@@ -15,6 +15,10 @@ const app = http.createServer(function(request, response){
         2. if로 routing한다.
         3. 페이지를 보여준다.
     */
+   /*
+     ToDo: http -> https(ssl)
+     ToDo: 템플릿으로 html코드생성, 회원가입생성
+   */
    let _url = request.url;
    let queryData = url.parse(_url, true).query;
    let pathname = url.parse(_url, true).pathname;
@@ -25,17 +29,17 @@ const app = http.createServer(function(request, response){
 
    if(pathname === '/' || pathname === '/index.html')
    {
-        response.writeHead(200);
+        response.writeHead(200, {'Content-Type': 'text/html'});
         response.end(fs.readFileSync(__dirname + '/public/index.html'));
    }
    else if(pathname === '/about.html')
    {
-        response.writeHead(200);
+        response.writeHead(200, {'Content-Type': 'text/html'});
         response.end(fs.readFileSync(__dirname + '/public/about.html'));
    }
    else if(pathname === '/board.html')
    {
-        response.writeHead(200);
+        response.writeHead(200, {'Content-Type': 'text/html'});
         response.end(fs.readFileSync(__dirname + '/public/board.html'));
    }
    else if(pathname === '/update_board.html')
@@ -48,7 +52,7 @@ const app = http.createServer(function(request, response){
    }
    else if(pathname === '/talk.html')
    {
-        response.writeHead(200);
+     response.writeHead(200, {'Content-Type': 'text/html'});
         response.end(fs.readFileSync(__dirname + '/public/talk.html'));
    }
    else if(pathname === '/update_talk.html')
@@ -61,7 +65,7 @@ const app = http.createServer(function(request, response){
    }
    else if(pathname === '/createDraw.html')
    {
-     response.writeHead(200);
+     response.writeHead(200, {'Content-Type': 'text/html'});
      response.end(fs.readFileSync(__dirname + '/public/createDraw.html'));
    }
    //static file: css, js, img
@@ -72,12 +76,12 @@ const app = http.createServer(function(request, response){
    }
    else if(path.extname(pathname) === '.js')
    {
-     response.writeHead(200, {'Content-Type': 'text/js'});
+     response.writeHead(200, {'Content-Type': 'text/javascript'});
      response.end(fs.readFileSync(`${__dirname}/public/js${pathname}`));
    }
    else if(path.dirname(pathname) === '/public/img')
    {
-     response.writeHead(200, {'Content-Type': 'text/img'});
+     response.writeHead(200, {'Content-Type': 'img/jpeg'});
      response.end(fs.readFileSync(__dirname + pathname));
    }
    //404: not found
