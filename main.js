87f9c4a9ab867731d74cb682184d8124845724f0
Joe <ckdfo1995@gmail.com>
2020-02-23 16:28:37 +0900

    migration node->express.js

--- main.js
+++ main.js
@@ -1,117 +1,120 @@
 const http = require('http');
-const url = require('url');
-const fs = require('fs');
-const path = require('path');
-const querystring = require('querystring');
-
+const fs = require('fs')
 const dbconfig = require('./dbconfig'); 
-const template = require('./lib/template');
-const create = require('./lib/create');
-
+const login = require('./lib/login');
 
 dbconfig.handleDisconnect();
 
+const express = require('express')
+const app = express()
 
-const app = http.createServer(function(request, response){
+const bodyParser = require('body-parser')
+const session = require('express-session')
+const compression = require('compression')
+
+const errRouter = require('./routes/errorHandler')
+
+app.set('views', './views')
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+//session, cookie
+
+// routing
+app.get('(/index.html|/)', (request, response) => {
+    const params = {
+        title: 'share Drawing', 
+        main: 'main1234',
+        aside: 'aside',
+        cssList: ['style', 'mainLayout', 'loginRes'], 
+        jsList: ['login']
+    }
+    response.render('main', params)
+})
+app.get('/about.html', (request, response) => {
+    const params = {
+        title: 'share Drawing-about', 
+        main: fs.readFileSync(__dirname + '/public/aboutMain.html'),
+        aside: 'aside',
+        cssList: ['style', 'mainLayout', 'loginRes', 'about'], 
+        jsList: ['login']
+    }
+    response.render('main', params)
+})
+
+app.get('/board.html', (request, response) => {
+    const params = {
+        title: 'share Drawing-Board', 
+        main: fs.readFileSync(__dirname + '/public/boardMain.html'),
+        aside: 'aside',
+        cssList: ['style', 'mainLayout', 'loginRes', 'talkBoard', 'board'], 
+        jsList: ['login']
+    }
+    response.render('main', params)
+})
+app.get('/update_board.html', (request, response) => {
+})
+app.get('/delete_board.html', (request, response) => {
+})
+
+
+app.get('/talk.html', (request, response) => {
+    const params = {
+        title: 'share Drawing-talkBoard', 
+        main: fs.readFileSync(__dirname + '/public/talkMain.html'),
+        aside: 'aside',
+        cssList: ['style', 'mainLayout', 'loginRes', 'talkBoard'], 
+        jsList: ['login']
+    }
+    response.render('main', params)
+})
+app.get('/update_talk.html', (request, response) => {
+
+})
+app.get('/delete_talk.html', (request, response) => {
+
+})
+
+app.get('/createDraw.html', (request, response) => {
+    const params = {
+        title: 'share Drawing-createDraw', 
+        main: fs.readFileSync(__dirname + '/public/createDrawMain.html'),
+        aside: 'aside',
+        cssList: ['style', 'mainLayout', 'loginRes'], 
+        jsList: ['canvasDraw', 'login']
+    }
+    response.render('main', params)
+})
+app.post('/login/register', (request, response) => {
     const db = dbconfig.getConnect();
-
-    /*
-        1. url을 가져온다.
-        2. if로 routing한다.
-        3. 페이지를 보여준다.
-    */
-    /*
-        ToDo: http -> https(ssl)
-        ToDo: 템플릿으로 html코드생성, 회원가입생성, 비번암호화(mysql, server, brower, 전송방식선택)
-        ToDo: mysql설치설정, 노드설치설정, 서버프로그램들을 배포를 위한 서버로 옮겨야함. 이것들을 간편히 하려면?
-    */
-    let _url = request.url;
-    let queryData = url.parse(_url, true).query;
-    let pathname = url.parse(_url, true).pathname;
-   
-    console.log('queryData:', queryData);
-    console.log('pathname:', pathname);
-
-    if(pathname === '/' || pathname === '/index.html')
-    {
-        create.index(request, response);
-    }
-    else if(pathname === '/about.html')
-    {
-        create.about(request, response);
-    }
-    else if(pathname === '/board.html')
-    {
-        create.board(request, response);
-    }
-    else if(pathname === '/update_board.html')
-    {
-
-    }
-    else if(pathname === '/delete_board.html')
-    {
-
-    }
-    else if(pathname === '/talk.html')
-    {
-        create.talk(request, response);
-    }
-    else if(pathname === '/update_talk.html')
-    {
-
-    }
-    else if(pathname === '/delete_talk.html')
-    {
-
-    }
-    else if(pathname === '/createDraw.html')
-    {
-        create.createDraw(request, response);
-    }
-    else if(pathname === '/login/register')
-    {
-        create.getRegister(request, response, db);
-    }
-    else if(pathname === '/login/login')
-    {
-        create.getLogin(request, response, db);
-    }
-    else if(pathname === '/ajax/redu/id')
-    {
-        create.postAjaxId(request, response, db);
-    }
-    else if(pathname === '/ajax/redu/nick')
-    {
-        create.postAjaxNick(request, response,db);
-    }
-    else if(pathname === '/ajax/redu/email')
-    {
-        create.postAjaxEmail(request, response,db);
-    }
-    //static file: css, js, img
-    else if(path.extname(pathname) === '.css')
-    {
-        response.writeHead(200, {'Content-Type': 'text/css'});
-        response.end(fs.readFileSync(`${__dirname}/public/css${pathname}`));
-    }
-    else if(path.extname(pathname) === '.js')
-    {
-        response.writeHead(200, {'Content-Type': 'text/javascript'});
-        response.end(fs.readFileSync(`${__dirname}/public/js${pathname}`));
-    }
-    else if(path.dirname(pathname) === '/public/img')
-    {
-        response.writeHead(200, {'Content-Type': 'img/jpeg'});
-        response.end(fs.readFileSync(__dirname + pathname));
-    }
-    //404: not found
-    else
-    {
-        response.writeHead(404);
-        response.end('Not found');
-    }
-
-});
+    login.getRegister(request, response, db);
+})
+app.post('/login/login', (request, response) => {
+    const db = dbconfig.getConnect();
+    login.getLogin(request, response, db);
+})
+app.post('/ajax/redu/id', (request, response) => {
+    const db = dbconfig.getConnect();
+    login.postAjaxId(request, response, db);
+})
+app.post('/ajax/redu/nick', (request, response) => {
+    const db = dbconfig.getConnect();
+    login.postAjaxNick(request, response,db);
+})
+app.post('/ajax/redu/email', (request, response) => {
+    const db = dbconfig.getConnect();
+    login.postAjaxEmail(request, response,db);
+})
 
 
-app.listen(3000);
+app.use(errRouter)
+module.exports = app
+
+
+/*
+    ToDo: http -> https(ssl)
+    ToDo: 템플릿으로 html코드생성, 회원가입생성, 비번암호화(mysql, server, brower, 전송방식선택)
+    ToDo: mysql설치설정, 노드설치설정, 서버프로그램들을 배포를 위한 서버로 옮겨야함. 이것들을 간편히 하려면?
+*/
